20505cd8ca1db3769538e1d7e55c19d408038347
alero <122855676+Alexander-Moldovan@users.noreply.github.com>
2023-08-31 21:17:58 -0300

    Code for MSP430

---
+++ Codigo/PT_PRUEBA_DE_RESISTENCIA/Debug/prueba_resistencia.d
@@ -0,0 +1,24 @@
+# FIXED
+
+prueba_resistencia.obj: ../prueba_resistencia.c
+prueba_resistencia.obj: C:/ti/ccs1230/ccs/ccs_base/msp430/include/msp430.h
+prueba_resistencia.obj: C:/ti/ccs1230/ccs/ccs_base/msp430/include/msp430fr2476.h
+prueba_resistencia.obj: C:/ti/ccs1230/ccs/ccs_base/msp430/include/in430.h
+prueba_resistencia.obj: C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+prueba_resistencia.obj: C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+prueba_resistencia.obj: C:/ti/ccs1230/ccs/ccs_base/msp430/include/legacy.h
+
+../prueba_resistencia.c:
+
+C:/ti/ccs1230/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1230/ccs/ccs_base/msp430/include/msp430fr2476.h:
+
+C:/ti/ccs1230/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs1230/ccs/ccs_base/msp430/include/legacy.h:
+
