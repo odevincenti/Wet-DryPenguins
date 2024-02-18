a92768abd2abe81d6eb6529d744320dea5c2aa55
alero <amoldovan+github@itba.edu.ar>
2024-02-18 15:12:28 -0300

    Added last version of Penguing Tracker Firmware

---
+++ ESP32/Codigo/penguin_tracker_base_06/penguin_tracker_base_06/not_an_include.h
@@ -0,0 +1,18 @@
+// aquí coloco los comentarios y demas q me pueden ser utiles mas tarde
+
+
+/*void discard_data_from_msp(void){
+  do{
+    while(MSP.available()){
+      MSP.read();
+    }
+    delay(10);
+  }while(MSP.available());
+}*/
+
+
+
+// TODO:
+// MODIFICAR PIN DE ENABLE TX!!!
+// testear con TODOS los numeros posibles para el tracking_buffer
+// Hacer un modo "experto" donde se puedan ingresar a mano los comandos
